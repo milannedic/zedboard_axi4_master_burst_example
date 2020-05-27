26e1b2f4cc736f9323e8547e080f39e4b35f421b
Milan Nedic <milan.nedic94@gmail.com>
2020-05-27 20:34:00 +0200

    verilog mode autotemplate example

--- hdl/axi4_master_burst_v1_0.v
+++ hdl/axi4_master_burst_v1_0.v
@@ -34,95 +34,95 @@
 
 
 		// Ports of Axi Slave Bus Interface S00_AXI
-		input wire  s00_axi_aclk,
-		input wire  s00_axi_aresetn,
-		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
-		input wire [2 : 0] s00_axi_awprot,
-		input wire  s00_axi_awvalid,
-		output wire  s00_axi_awready,
-		input wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
+		input wire                                  s00_axi_aclk,
+		input wire                                  s00_axi_aresetn,
+		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0]     s00_axi_awaddr,
+		input wire [2 : 0]                          s00_axi_awprot,
+		input wire                                  s00_axi_awvalid,
+		output wire                                 s00_axi_awready,
+		input wire [C_S00_AXI_DATA_WIDTH-1 : 0]     s00_axi_wdata,
 		input wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
-		input wire  s00_axi_wvalid,
-		output wire  s00_axi_wready,
-		output wire [1 : 0] s00_axi_bresp,
-		output wire  s00_axi_bvalid,
-		input wire  s00_axi_bready,
-		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
-		input wire [2 : 0] s00_axi_arprot,
-		input wire  s00_axi_arvalid,
-		output wire  s00_axi_arready,
-		output wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
-		output wire [1 : 0] s00_axi_rresp,
-		output wire  s00_axi_rvalid,
-		input wire  s00_axi_rready,
+		input wire                                  s00_axi_wvalid,
+		output wire                                 s00_axi_wready,
+		output wire [1 : 0]                         s00_axi_bresp,
+		output wire                                 s00_axi_bvalid,
+		input wire                                  s00_axi_bready,
+		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0]     s00_axi_araddr,
+		input wire [2 : 0]                          s00_axi_arprot,
+		input wire                                  s00_axi_arvalid,
+		output wire                                 s00_axi_arready,
+		output wire [C_S00_AXI_DATA_WIDTH-1 : 0]    s00_axi_rdata,
+		output wire [1 : 0]                         s00_axi_rresp,
+		output wire                                 s00_axi_rvalid,
+		input wire                                  s00_axi_rready,
 
 		// Ports of Axi Master Bus Interface M00_AXI
 		// input wire  m00_axi_init_axi_txn,
 		// output wire  m00_axi_txn_done,
 		// output wire  m00_axi_error,
-		input wire  m00_axi_aclk,
-		input wire  m00_axi_aresetn,
-		output wire [C_M00_AXI_ID_WIDTH-1 : 0] m00_axi_awid,
-		output wire [C_M00_AXI_ADDR_WIDTH-1 : 0] m00_axi_awaddr,
-		output wire [7 : 0] m00_axi_awlen,
-		output wire [2 : 0] m00_axi_awsize,
-		output wire [1 : 0] m00_axi_awburst,
-		output wire  m00_axi_awlock,
-		output wire [3 : 0] m00_axi_awcache,
-		output wire [2 : 0] m00_axi_awprot,
-		output wire [3 : 0] m00_axi_awqos,
-		output wire [C_M00_AXI_AWUSER_WIDTH-1 : 0] m00_axi_awuser,
-		output wire  m00_axi_awvalid,
-		input wire  m00_axi_awready,
-		output wire [C_M00_AXI_DATA_WIDTH-1 : 0] m00_axi_wdata,
-		output wire [C_M00_AXI_DATA_WIDTH/8-1 : 0] m00_axi_wstrb,
-		output wire  m00_axi_wlast,
-		output wire [C_M00_AXI_WUSER_WIDTH-1 : 0] m00_axi_wuser,
-		output wire  m00_axi_wvalid,
-		input wire  m00_axi_wready,
-		input wire [C_M00_AXI_ID_WIDTH-1 : 0] m00_axi_bid,
-		input wire [1 : 0] m00_axi_bresp,
-		input wire [C_M00_AXI_BUSER_WIDTH-1 : 0] m00_axi_buser,
-		input wire  m00_axi_bvalid,
-		output wire  m00_axi_bready,
-		output wire [C_M00_AXI_ID_WIDTH-1 : 0] m00_axi_arid,
-		output wire [C_M00_AXI_ADDR_WIDTH-1 : 0] m00_axi_araddr,
-		output wire [7 : 0] m00_axi_arlen,
-		output wire [2 : 0] m00_axi_arsize,
-		output wire [1 : 0] m00_axi_arburst,
-		output wire  m00_axi_arlock,
-		output wire [3 : 0] m00_axi_arcache,
-		output wire [2 : 0] m00_axi_arprot,
-		output wire [3 : 0] m00_axi_arqos,
-		output wire [C_M00_AXI_ARUSER_WIDTH-1 : 0] m00_axi_aruser,
-		output wire  m00_axi_arvalid,
-		input wire  m00_axi_arready,
-		input wire [C_M00_AXI_ID_WIDTH-1 : 0] m00_axi_rid,
-		input wire [C_M00_AXI_DATA_WIDTH-1 : 0] m00_axi_rdata,
-		input wire [1 : 0] m00_axi_rresp,
-		input wire  m00_axi_rlast,
-		input wire [C_M00_AXI_RUSER_WIDTH-1 : 0] m00_axi_ruser,
-		input wire  m00_axi_rvalid,
-		output wire  m00_axi_rready
+		input wire                                  m00_axi_aclk,
+		input wire                                  m00_axi_aresetn,
+		output wire [C_M00_AXI_ID_WIDTH-1 : 0]      m00_axi_awid,
+		output wire [C_M00_AXI_ADDR_WIDTH-1 : 0]    m00_axi_awaddr,
+		output wire [7 : 0]                         m00_axi_awlen,
+		output wire [2 : 0]                         m00_axi_awsize,
+		output wire [1 : 0]                         m00_axi_awburst,
+		output wire                                 m00_axi_awlock,
+		output wire [3 : 0]                         m00_axi_awcache,
+		output wire [2 : 0]                         m00_axi_awprot,
+		output wire [3 : 0]                         m00_axi_awqos,
+		output wire [C_M00_AXI_AWUSER_WIDTH-1 : 0]  m00_axi_awuser,
+		output wire                                 m00_axi_awvalid,
+		input wire                                  m00_axi_awready,
+		output wire [C_M00_AXI_DATA_WIDTH-1 : 0]    m00_axi_wdata,
+		output wire [C_M00_AXI_DATA_WIDTH/8-1 : 0]  m00_axi_wstrb,
+		output wire                                 m00_axi_wlast,
+		output wire [C_M00_AXI_WUSER_WIDTH-1 : 0]   m00_axi_wuser,
+		output wire                                 m00_axi_wvalid,
+		input wire                                  m00_axi_wready,
+		input wire [C_M00_AXI_ID_WIDTH-1 : 0]       m00_axi_bid,
+		input wire [1 : 0]                          m00_axi_bresp,
+		input wire [C_M00_AXI_BUSER_WIDTH-1 : 0]    m00_axi_buser,
+		input wire                                  m00_axi_bvalid,
+		output wire                                 m00_axi_bready,
+		output wire [C_M00_AXI_ID_WIDTH-1 : 0]      m00_axi_arid,
+		output wire [C_M00_AXI_ADDR_WIDTH-1 : 0]    m00_axi_araddr,
+		output wire [7 : 0]                         m00_axi_arlen,
+		output wire [2 : 0]                         m00_axi_arsize,
+		output wire [1 : 0]                         m00_axi_arburst,
+		output wire                                 m00_axi_arlock,
+		output wire [3 : 0]                         m00_axi_arcache,
+		output wire [2 : 0]                         m00_axi_arprot,
+		output wire [3 : 0]                         m00_axi_arqos,
+		output wire [C_M00_AXI_ARUSER_WIDTH-1 : 0]  m00_axi_aruser,
+		output wire                                 m00_axi_arvalid,
+		input wire                                  m00_axi_arready,
+		input wire [C_M00_AXI_ID_WIDTH-1 : 0]       m00_axi_rid,
+		input wire [C_M00_AXI_DATA_WIDTH-1 : 0]     m00_axi_rdata,
+		input wire [1 : 0]                          m00_axi_rresp,
+		input wire                                  m00_axi_rlast,
+		input wire [C_M00_AXI_RUSER_WIDTH-1 : 0]    m00_axi_ruser,
+		input wire                                  m00_axi_rvalid,
+		output wire                                 m00_axi_rready
 	);
-	
+
 	wire m00_axi_init_axi_txn;
     wire m00_axi_txn_done;
     wire m00_axi_error;
-    
+
 // Instantiation of Axi Bus Interface S00_AXI
-	axi4_master_burst_v1_0_S00_AXI # ( 
+	axi4_master_burst_v1_0_S00_AXI # (
 		.C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
 		.C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
 	) axi4_master_burst_v1_0_S00_AXI_inst (
 		.S_AXI_ACLK(s00_axi_aclk),
 		.S_AXI_ARESETN(s00_axi_aresetn),
-		
+
 		// User-added ports
 		.init_txn(m00_axi_init_axi_txn),
         .txn_done(m00_axi_txn_done),
         .txn_error(m00_axi_error),
-		
+
 		.S_AXI_AWADDR(s00_axi_awaddr),
 		.S_AXI_AWPROT(s00_axi_awprot),
 		.S_AXI_AWVALID(s00_axi_awvalid),
@@ -145,7 +145,7 @@
 	);
 
 // Instantiation of Axi Bus Interface M00_AXI
-	axi4_master_burst_v1_0_M00_AXI # ( 
+	axi4_master_burst_v1_0_M00_AXI # (
 		.C_M_TARGET_SLAVE_BASE_ADDR(C_M00_AXI_TARGET_SLAVE_BASE_ADDR),
 		.C_M_AXI_BURST_LEN(C_M00_AXI_BURST_LEN),
 		.C_M_AXI_ID_WIDTH(C_M00_AXI_ID_WIDTH),
@@ -205,6 +205,93 @@
 		.M_AXI_RVALID(m00_axi_rvalid),
 		.M_AXI_RREADY(m00_axi_rready)
 	);
+
+   /* axi4_master_burst_v1_0_M00_AXI AUTO_TEMPLATE (
+		.C_M_TARGET_SLAVE_BASE_ADDR(C_M00_AXI_TARGET_SLAVE_BASE_ADDR),
+		.C_M_AXI_BURST_LEN(C_M00_AXI_BURST_LEN),
+		.C_M_AXI_ID_WIDTH(C_M00_AXI_ID_WIDTH),
+		.C_M_AXI_ADDR_WIDTH(C_M00_AXI_ADDR_WIDTH),
+		.C_M_AXI_DATA_WIDTH(C_M00_AXI_DATA_WIDTH),
+		.C_M_AXI_AWUSER_WIDTH(C_M00_AXI_AWUSER_WIDTH),
+		.C_M_AXI_ARUSER_WIDTH(C_M00_AXI_ARUSER_WIDTH),
+		.C_M_AXI_WUSER_WIDTH(C_M00_AXI_WUSER_WIDTH),
+		.C_M_AXI_RUSER_WIDTH(C_M00_AXI_RUSER_WIDTH),
+		.C_M_AXI_BUSER_WIDTH(C_M00_AXI_BUSER_WIDTH),
+
+    .M_AXI_\(.*\) (m00_axi_@"(downcase (substring vl-name 6))"[]),
+    .\(.*\) (m00_axi_@"(downcase vl-name)"[]),
+
+    // .ERROR (m00_axi_error),
+    // .TXN_DONE (m00_axi_txn_done),
+    // .TXN_ERROR (m00_axi_error),
+    // .INIT_AXI_TXN (m00_axi_init_axi_txn),
+    // .\(.*\) (@"(downcase vl-name)"[]), --> downcase the whole name
+
+    )*/
+	 axi4_master_burst_v1_0_M00_AXI
+   # (
+      // Parameters
+      .C_M_TARGET_SLAVE_BASE_ADDR       (C_M00_AXI_TARGET_SLAVE_BASE_ADDR),
+      .C_M_AXI_BURST_LEN                (C_M00_AXI_BURST_LEN),
+      .C_M_AXI_ID_WIDTH                 (C_M00_AXI_ID_WIDTH),
+      .C_M_AXI_ADDR_WIDTH               (C_M00_AXI_ADDR_WIDTH),
+      .C_M_AXI_DATA_WIDTH               (C_M00_AXI_DATA_WIDTH),
+      .C_M_AXI_AWUSER_WIDTH             (C_M00_AXI_AWUSER_WIDTH),
+      .C_M_AXI_ARUSER_WIDTH             (C_M00_AXI_ARUSER_WIDTH),
+      .C_M_AXI_WUSER_WIDTH              (C_M00_AXI_WUSER_WIDTH),
+      .C_M_AXI_RUSER_WIDTH              (C_M00_AXI_RUSER_WIDTH),
+      .C_M_AXI_BUSER_WIDTH              (C_M00_AXI_BUSER_WIDTH))
+   I_axi4_master_burst_v1_0_M00_AXI
+     (/*AUTOINST*/
+      // Outputs
+      .TXN_DONE                         (m00_axi_txn_done),      // Templated
+      .ERROR                            (m00_axi_error),         // Templated
+      .M_AXI_AWID                       (m00_axi_awid[C_M_AXI_ID_WIDTH-1:0]), // Templated
+      .M_AXI_AWADDR                     (m00_axi_awaddr[C_M_AXI_ADDR_WIDTH-1:0]), // Templated
+      .M_AXI_AWLEN                      (m00_axi_awlen[7:0]),    // Templated
+      .M_AXI_AWSIZE                     (m00_axi_awsize[2:0]),   // Templated
+      .M_AXI_AWBURST                    (m00_axi_awburst[1:0]),  // Templated
+      .M_AXI_AWLOCK                     (m00_axi_awlock),        // Templated
+      .M_AXI_AWCACHE                    (m00_axi_awcache[3:0]),  // Templated
+      .M_AXI_AWPROT                     (m00_axi_awprot[2:0]),   // Templated
+      .M_AXI_AWQOS                      (m00_axi_awqos[3:0]),    // Templated
+      .M_AXI_AWUSER                     (m00_axi_awuser[C_M_AXI_AWUSER_WIDTH-1:0]), // Templated
+      .M_AXI_AWVALID                    (m00_axi_awvalid),       // Templated
+      .M_AXI_WDATA                      (m00_axi_wdata[C_M_AXI_DATA_WIDTH-1:0]), // Templated
+      .M_AXI_WSTRB                      (m00_axi_wstrb[C_M_AXI_DATA_WIDTH/8-1:0]), // Templated
+      .M_AXI_WLAST                      (m00_axi_wlast),         // Templated
+      .M_AXI_WUSER                      (m00_axi_wuser[C_M_AXI_WUSER_WIDTH-1:0]), // Templated
+      .M_AXI_WVALID                     (m00_axi_wvalid),        // Templated
+      .M_AXI_BREADY                     (m00_axi_bready),        // Templated
+      .M_AXI_ARID                       (m00_axi_arid[C_M_AXI_ID_WIDTH-1:0]), // Templated
+      .M_AXI_ARADDR                     (m00_axi_araddr[C_M_AXI_ADDR_WIDTH-1:0]), // Templated
+      .M_AXI_ARLEN                      (m00_axi_arlen[7:0]),    // Templated
+      .M_AXI_ARSIZE                     (m00_axi_arsize[2:0]),   // Templated
+      .M_AXI_ARBURST                    (m00_axi_arburst[1:0]),  // Templated
+      .M_AXI_ARLOCK                     (m00_axi_arlock),        // Templated
+      .M_AXI_ARCACHE                    (m00_axi_arcache[3:0]),  // Templated
+      .M_AXI_ARPROT                     (m00_axi_arprot[2:0]),   // Templated
+      .M_AXI_ARQOS                      (m00_axi_arqos[3:0]),    // Templated
+      .M_AXI_ARUSER                     (m00_axi_aruser[C_M_AXI_ARUSER_WIDTH-1:0]), // Templated
+      .M_AXI_ARVALID                    (m00_axi_arvalid),       // Templated
+      .M_AXI_RREADY                     (m00_axi_rready),        // Templated
+      // Inputs
+      .INIT_AXI_TXN                     (m00_axi_init_axi_txn),  // Templated
+      .M_AXI_ACLK                       (m00_axi_aclk),          // Templated
+      .M_AXI_ARESETN                    (m00_axi_aresetn),       // Templated
+      .M_AXI_AWREADY                    (m00_axi_awready),       // Templated
+      .M_AXI_WREADY                     (m00_axi_wready),        // Templated
+      .M_AXI_BID                        (m00_axi_bid[C_M_AXI_ID_WIDTH-1:0]), // Templated
+      .M_AXI_BRESP                      (m00_axi_bresp[1:0]),    // Templated
+      .M_AXI_BUSER                      (m00_axi_buser[C_M_AXI_BUSER_WIDTH-1:0]), // Templated
+      .M_AXI_BVALID                     (m00_axi_bvalid),        // Templated
+      .M_AXI_ARREADY                    (m00_axi_arready),       // Templated
+      .M_AXI_RID                        (m00_axi_rid[C_M_AXI_ID_WIDTH-1:0]), // Templated
+      .M_AXI_RDATA                      (m00_axi_rdata[C_M_AXI_DATA_WIDTH-1:0]), // Templated
+      .M_AXI_RRESP                      (m00_axi_rresp[1:0]),    // Templated
+      .M_AXI_RLAST                      (m00_axi_rlast),         // Templated
+      .M_AXI_RUSER                      (m00_axi_ruser[C_M_AXI_RUSER_WIDTH-1:0]), // Templated
+      .M_AXI_RVALID                     (m00_axi_rvalid));        // Templated
 
 	// Add user logic here
 
--- hdl/axi4_master_burst_v1_0_S00_AXI.v
+++ hdl/axi4_master_burst_v1_0_S00_AXI.v
@@ -19,7 +19,7 @@
         input wire txn_done,
         input wire txn_error,
         // User ports ends
-        
+
 		// Do not modify the ports beyond this line
 
 		// Global Clock Signal
@@ -38,11 +38,11 @@
 		// Write address ready. This signal indicates that the slave is ready
     		// to accept an address and associated control signals.
 		output wire  S_AXI_AWREADY,
-		// Write data (issued by master, acceped by Slave) 
+		// Write data (issued by master, acceped by Slave)
 		input wire [C_S_AXI_DATA_WIDTH-1 : 0] S_AXI_WDATA,
 		// Write strobes. This signal indicates which byte lanes hold
     		// valid data. There is one write strobe bit for each eight
-    		// bits of the write data bus.    
+    		// bits of the write data bus.
 		input wire [(C_S_AXI_DATA_WIDTH/8)-1 : 0] S_AXI_WSTRB,
 		// Write valid. This signal indicates that valid write
     		// data and strobes are available.
@@ -85,37 +85,37 @@
 	);
 
 	// AXI4LITE signals
-	reg [C_S_AXI_ADDR_WIDTH-1 : 0] 	axi_awaddr;
-	reg  	axi_awready;
-	reg  	axi_wready;
-	reg [1 : 0] 	axi_bresp;
-	reg  	axi_bvalid;
-	reg [C_S_AXI_ADDR_WIDTH-1 : 0] 	axi_araddr;
-	reg  	axi_arready;
-	reg [C_S_AXI_DATA_WIDTH-1 : 0] 	axi_rdata;
-	reg [1 : 0] 	axi_rresp;
-	reg  	axi_rvalid;
+	 reg [C_S_AXI_ADDR_WIDTH-1 : 0] axi_awaddr;
+	 reg                            axi_awready;
+	 reg                            axi_wready;
+	 reg [1 : 0]                    axi_bresp;
+	 reg                            axi_bvalid;
+	 reg [C_S_AXI_ADDR_WIDTH-1 : 0] axi_araddr;
+	 reg                            axi_arready;
+	 reg [C_S_AXI_DATA_WIDTH-1 : 0] axi_rdata;
+	 reg [1 : 0]                    axi_rresp;
+	 reg                            axi_rvalid;
 
 	// Example-specific design signals
 	// local parameter for addressing 32 bit / 64 bit C_S_AXI_DATA_WIDTH
 	// ADDR_LSB is used for addressing 32/64 bit registers/memories
 	// ADDR_LSB = 2 for 32 bits (n downto 2)
 	// ADDR_LSB = 3 for 64 bits (n downto 3)
-	localparam integer ADDR_LSB = (C_S_AXI_DATA_WIDTH/32) + 1;
-	localparam integer OPT_MEM_ADDR_BITS = 1;
+	 localparam integer             ADDR_LSB = (C_S_AXI_DATA_WIDTH/32) + 1;
+	 localparam integer             OPT_MEM_ADDR_BITS = 1;
 	//----------------------------------------------
 	//-- Signals for user logic register space example
 	//------------------------------------------------
 	//-- Number of Slave Registers 4
-	reg [C_S_AXI_DATA_WIDTH-1:0]	slv_reg0;
-	reg [C_S_AXI_DATA_WIDTH-1:0]	slv_reg1;
-	reg [C_S_AXI_DATA_WIDTH-1:0]	slv_reg2;
-	reg [C_S_AXI_DATA_WIDTH-1:0]	slv_reg3;
-	wire	 slv_reg_rden;
-	wire	 slv_reg_wren;
-	reg [C_S_AXI_DATA_WIDTH-1:0]	 reg_data_out;
-	integer	 byte_index;
-	reg	 aw_en;
+	 reg [C_S_AXI_DATA_WIDTH-1:0]   slv_reg0;
+	 reg [C_S_AXI_DATA_WIDTH-1:0]   slv_reg1;
+	 reg [C_S_AXI_DATA_WIDTH-1:0]   slv_reg2;
+	 reg [C_S_AXI_DATA_WIDTH-1:0]   slv_reg3;
+	 wire                           slv_reg_rden;
+	 wire                           slv_reg_wren;
+	 reg [C_S_AXI_DATA_WIDTH-1:0]   reg_data_out;
+	 integer                        byte_index;
+	 reg                            aw_en;
 
 	// I/O Connections assignments
 
@@ -138,15 +138,15 @@
 	    begin
 	      axi_awready <= 1'b0;
 	      aw_en <= 1'b1;
-	    end 
+	    end
 	  else
-	    begin    
+	    begin
 	      if (~axi_awready && S_AXI_AWVALID && S_AXI_WVALID && aw_en)
 	        begin
-	          // slave is ready to accept write address when 
+	          // slave is ready to accept write address when
 	          // there is a valid write address and write data
-	          // on the write address and data bus. This design 
-	          // expects no outstanding transactions. 
+	          // on the write address and data bus. This design
+	          // expects no outstanding transactions.
 	          axi_awready <= 1'b1;
 	          aw_en <= 1'b0;
 	        end
@@ -155,60 +155,60 @@
 	              aw_en <= 1'b1;
 	              axi_awready <= 1'b0;
 	            end
-	      else           
+	      else
 	        begin
 	          axi_awready <= 1'b0;
 	        end
-	    end 
-	end       
+	    end
+	end
 
 	// Implement axi_awaddr latching
-	// This process is used to latch the address when both 
-	// S_AXI_AWVALID and S_AXI_WVALID are valid. 
+	// This process is used to latch the address when both
+	// S_AXI_AWVALID and S_AXI_WVALID are valid.
 
 	always @( posedge S_AXI_ACLK )
 	begin
 	  if ( S_AXI_ARESETN == 1'b0 )
 	    begin
 	      axi_awaddr <= 0;
-	    end 
+	    end
 	  else
-	    begin    
+	    begin
 	      if (~axi_awready && S_AXI_AWVALID && S_AXI_WVALID && aw_en)
 	        begin
-	          // Write Address latching 
+	          // Write Address latching
 	          axi_awaddr <= S_AXI_AWADDR;
 	        end
-	    end 
-	end       
+	    end
+	end
 
 	// Implement axi_wready generation
 	// axi_wready is asserted for one S_AXI_ACLK clock cycle when both
-	// S_AXI_AWVALID and S_AXI_WVALID are asserted. axi_wready is 
-	// de-asserted when reset is low. 
+	// S_AXI_AWVALID and S_AXI_WVALID are asserted. axi_wready is
+	// de-asserted when reset is low.
 
 	always @( posedge S_AXI_ACLK )
 	begin
 	  if ( S_AXI_ARESETN == 1'b0 )
 	    begin
 	      axi_wready <= 1'b0;
-	    end 
+	    end
 	  else
-	    begin    
+	    begin
 	      if (~axi_wready && S_AXI_WVALID && S_AXI_AWVALID && aw_en )
 	        begin
-	          // slave is ready to accept write data when 
+	          // slave is ready to accept write data when
 	          // there is a valid write address and write data
-	          // on the write address and data bus. This design 
-	          // expects no outstanding transactions. 
+	          // on the write address and data bus. This design
+	          // expects no outstanding transactions.
 	          axi_wready <= 1'b1;
 	        end
 	      else
 	        begin
 	          axi_wready <= 1'b0;
 	        end
-	    end 
-	end       
+	    end
+	end
 
 	// Implement memory mapped register select and write logic generation
 	// The write data is accepted and written to memory mapped registers when
@@ -227,7 +227,7 @@
 	      slv_reg1 <= 0;
 	      slv_reg2 <= 0;
 	      slv_reg3 <= 0;
-	    end 
+	    end
 	  else begin
 	    if (slv_reg_wren)
 	      begin
@@ -235,31 +235,31 @@
 	          2'h0:
 	            for ( byte_index = 0; byte_index <= (C_S_AXI_DATA_WIDTH/8)-1; byte_index = byte_index+1 )
 	              if ( S_AXI_WSTRB[byte_index] == 1 ) begin
-	                // Respective byte enables are asserted as per write strobes 
+	                // Respective byte enables are asserted as per write strobes
 	                // Slave register 0
 	                slv_reg0[(byte_index*8) +: 8] <= S_AXI_WDATA[(byte_index*8) +: 8];
-	              end  
+	              end
 	          2'h1:
 	            for ( byte_index = 0; byte_index <= (C_S_AXI_DATA_WIDTH/8)-1; byte_index = byte_index+1 )
 	              if ( S_AXI_WSTRB[byte_index] == 1 ) begin
-	                // Respective byte enables are asserted as per write strobes 
+	                // Respective byte enables are asserted as per write strobes
 	                // Slave register 1
 	                slv_reg1[(byte_index*8) +: 8] <= S_AXI_WDATA[(byte_index*8) +: 8];
-	              end  
+	              end
 	          2'h2:
 	            for ( byte_index = 0; byte_index <= (C_S_AXI_DATA_WIDTH/8)-1; byte_index = byte_index+1 )
 	              if ( S_AXI_WSTRB[byte_index] == 1 ) begin
-	                // Respective byte enables are asserted as per write strobes 
+	                // Respective byte enables are asserted as per write strobes
 	                // Slave register 2
 	                slv_reg2[(byte_index*8) +: 8] <= S_AXI_WDATA[(byte_index*8) +: 8];
-	              end  
+	              end
 	          2'h3:
 	            for ( byte_index = 0; byte_index <= (C_S_AXI_DATA_WIDTH/8)-1; byte_index = byte_index+1 )
 	              if ( S_AXI_WSTRB[byte_index] == 1 ) begin
-	                // Respective byte enables are asserted as per write strobes 
+	                // Respective byte enables are asserted as per write strobes
 	                // Slave register 3
 	                slv_reg3[(byte_index*8) +: 8] <= S_AXI_WDATA[(byte_index*8) +: 8];
-	              end  
+	              end
 	          default : begin
 	                      slv_reg0 <= slv_reg0;
 	                      slv_reg1 <= slv_reg1;
@@ -269,12 +269,12 @@
 	        endcase
 	      end
 	  end
-	end    
+	end
 
 	// Implement write response logic generation
-	// The write response and response valid signals are asserted by the slave 
-	// when axi_wready, S_AXI_WVALID, axi_wready and S_AXI_WVALID are asserted.  
-	// This marks the acceptance of address and indicates the status of 
+	// The write response and response valid signals are asserted by the slave
+	// when axi_wready, S_AXI_WVALID, axi_wready and S_AXI_WVALID are asserted.
+	// This marks the acceptance of address and indicates the status of
 	// write transaction.
 
 	always @( posedge S_AXI_ACLK )
@@ -283,32 +283,32 @@
 	    begin
 	      axi_bvalid  <= 0;
 	      axi_bresp   <= 2'b0;
-	    end 
+	    end
 	  else
-	    begin    
+	    begin
 	      if (axi_awready && S_AXI_AWVALID && ~axi_bvalid && axi_wready && S_AXI_WVALID)
 	        begin
 	          // indicates a valid write response is available
 	          axi_bvalid <= 1'b1;
-	          axi_bresp  <= 2'b0; // 'OKAY' response 
+	          axi_bresp  <= 2'b0; // 'OKAY' response
 	        end                   // work error responses in future
 	      else
 	        begin
-	          if (S_AXI_BREADY && axi_bvalid) 
-	            //check if bready is asserted while bvalid is high) 
-	            //(there is a possibility that bready is always asserted high)   
+	          if (S_AXI_BREADY && axi_bvalid)
+	            //check if bready is asserted while bvalid is high)
+	            //(there is a possibility that bready is always asserted high)
 	            begin
-	              axi_bvalid <= 1'b0; 
-	            end  
+	              axi_bvalid <= 1'b0;
+	            end
 	        end
 	    end
-	end   
+	end
 
 	// Implement axi_arready generation
 	// axi_arready is asserted for one S_AXI_ACLK clock cycle when
-	// S_AXI_ARVALID is asserted. axi_awready is 
-	// de-asserted when reset (active low) is asserted. 
-	// The read address is also latched when S_AXI_ARVALID is 
+	// S_AXI_ARVALID is asserted. axi_awready is
+	// de-asserted when reset (active low) is asserted.
+	// The read address is also latched when S_AXI_ARVALID is
 	// asserted. axi_araddr is reset to zero on reset assertion.
 
 	always @( posedge S_AXI_ACLK )
@@ -317,9 +317,9 @@
 	    begin
 	      axi_arready <= 1'b0;
 	      axi_araddr  <= 32'b0;
-	    end 
+	    end
 	  else
-	    begin    
+	    begin
 	      if (~axi_arready && S_AXI_ARVALID)
 	        begin
 	          // indicates that the slave has acceped the valid read address
@@ -331,39 +331,39 @@
 	        begin
 	          axi_arready <= 1'b0;
 	        end
-	    end 
-	end       
+	    end
+	end
 
 	// Implement axi_arvalid generation
-	// axi_rvalid is asserted for one S_AXI_ACLK clock cycle when both 
-	// S_AXI_ARVALID and axi_arready are asserted. The slave registers 
-	// data are available on the axi_rdata bus at this instance. The 
-	// assertion of axi_rvalid marks the validity of read data on the 
-	// bus and axi_rresp indicates the status of read transaction.axi_rvalid 
-	// is deasserted on reset (active low). axi_rresp and axi_rdata are 
-	// cleared to zero on reset (active low).  
+	// axi_rvalid is asserted for one S_AXI_ACLK clock cycle when both
+	// S_AXI_ARVALID and axi_arready are asserted. The slave registers
+	// data are available on the axi_rdata bus at this instance. The
+	// assertion of axi_rvalid marks the validity of read data on the
+	// bus and axi_rresp indicates the status of read transaction.axi_rvalid
+	// is deasserted on reset (active low). axi_rresp and axi_rdata are
+	// cleared to zero on reset (active low).
 	always @( posedge S_AXI_ACLK )
 	begin
 	  if ( S_AXI_ARESETN == 1'b0 )
 	    begin
 	      axi_rvalid <= 0;
 	      axi_rresp  <= 0;
-	    end 
+	    end
 	  else
-	    begin    
+	    begin
 	      if (axi_arready && S_AXI_ARVALID && ~axi_rvalid)
 	        begin
 	          // Valid read data is available at the read data bus
 	          axi_rvalid <= 1'b1;
 	          axi_rresp  <= 2'b0; // 'OKAY' response
-	        end   
+	        end
 	      else if (axi_rvalid && S_AXI_RREADY)
 	        begin
 	          // Read data is accepted by the master
 	          axi_rvalid <= 1'b0;
-	        end                
+	        end
 	    end
-	end    
+	end
 
 /*
 	// Implement memory mapped register select and read logic generation
@@ -403,18 +403,18 @@ end
 	  if ( S_AXI_ARESETN == 1'b0 )
 	    begin
 	      axi_rdata  <= 0;
-	    end 
+	    end
 	  else
-	    begin    
-	      // When there is a valid read address (S_AXI_ARVALID) with 
-	      // acceptance of read address by the slave (axi_arready), 
-	      // output the read dada 
+	    begin
+	      // When there is a valid read address (S_AXI_ARVALID) with
+	      // acceptance of read address by the slave (axi_arready),
+	      // output the read dada
 	      if (slv_reg_rden)
 	        begin
 	          axi_rdata <= reg_data_out;     // register read data
-	        end   
+	        end
 	    end
-	end    
+	end
 
 
 	// Add user logic here
